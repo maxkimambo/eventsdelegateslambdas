29f1253884936e18ad7396129a246cbd858cca37
Max Kimambo <max@kimambo.de>
2015-09-21 21:46:42 +0200

    Async Delegates Demo complete

--- AsyncDelegates/Form1.cs
+++ AsyncDelegates/Form1.cs
@@ -26,7 +26,7 @@ namespace AsyncDelegates
         {
             var max = 100;
             var updateStatus = new StatusUpdateDelegate(StartProcess);
-            // this doesnt work because UI thread is being updated from 
+            // this doesnt work because if UI thread is being updated from 
             // a secondary thread. 
             
             updateStatus.BeginInvoke(max, null, null); 
@@ -47,6 +47,12 @@ namespace AsyncDelegates
 
         private void ShowProgress(int i)
         {
+
+            // here we check if the background thread tries to update an element on the 
+            // gui thread if yes 
+            // we redirect that activity to the ui thread by 
+            // reinvocoking showProgress function using this (which is the form) 
+            // then everything works as its supposed to be. 
             if (lblProgress.InvokeRequired)
             {
                 var showProgressDel = new ShowProgressDelegate(ShowProgress);
